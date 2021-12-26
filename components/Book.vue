558b6fa653442da1b234f07f01d268cc37eca22b
Tyler Trotter <tylertrotter@gmail.com>
2021-12-26 12:21:37 -0500

    Create data file

--- components/Book.vue
+++ components/Book.vue
@@ -1,8 +1,8 @@
 <template>
 		<div>
-				<h1>{{ name.toUpperCase() }}</h1>
+				<h1>{{ friendlyName }}</h1>
 				<ol>
-						<li v-for="chapter in books[name.toLowerCase()]" :key="chapter">
+						<li v-for="chapter in books[friendlyName]" :key="chapter">
 							<h2>Chapter {{ chapter.chapter }}</h2>
 							<ol>
 								<li v-for="study in chapter.studies" :key="study.title">
@@ -15,8 +15,7 @@
 </template>
 
 <script>
-	import genesis from '/data/genesis';
-	import james from '/data/james';
+	import books from '/data/books';
 	
 	export default {
 			name: 'Book',
@@ -28,7 +27,8 @@
 			},
 			data(){
 				return {
-					books: { genesis, james }
+					books: books,
+					friendlyName: this.name.replace(/-/g, ' ').replace(/(^\w|\s\w)/g, w => w.toUpperCase())
 				}
 			}
 	}
--- components/Menu.vue
+++ components/Menu.vue
@@ -29,7 +29,33 @@
 					<section>
 						<h2>New Testament</h2>
 						<ol>
+							<li><NuxtLink to="/revelation">Revelation</NuxtLink></li>
+							<li><NuxtLink to="/jude">Jude</NuxtLink></li>
+							<li><NuxtLink to="/3-john">3 John</NuxtLink></li>
+							<li><NuxtLink to="/2-john">2 John</NuxtLink></li>
+							<li><NuxtLink to="/1-john">1 John</NuxtLink></li>
+							<li><NuxtLink to="/2-peter">2 Peter</NuxtLink></li>
+							<li><NuxtLink to="/1-peter">1 Peter</NuxtLink></li>
 							<li><NuxtLink to="/james">James</NuxtLink></li>
+							<li><NuxtLink to="/hebrews">Hebrews</NuxtLink></li>
+							<li><NuxtLink to="/philemon">Philemon</NuxtLink></li>
+							<li><NuxtLink to="/titus">Titus</NuxtLink></li>
+							<li><NuxtLink to="/2-timothy">2 Timothy</NuxtLink></li>
+							<li><NuxtLink to="/1-timothy">1 Timothy</NuxtLink></li>
+							<li><NuxtLink to="/2-thessalonians">2 Thessalonians</NuxtLink></li>
+							<li><NuxtLink to="/1-thessalonians">1 Thessalonians</NuxtLink></li>
+							<li><NuxtLink to="/colossians">Colossians</NuxtLink></li>
+							<li><NuxtLink to="/philippians">Philippians</NuxtLink></li>
+							<li><NuxtLink to="/ephesians">Ephesians</NuxtLink></li>
+							<li><NuxtLink to="/galatians">Galatians</NuxtLink></li>
+							<li><NuxtLink to="/2-corinthians">2 Corinthians</NuxtLink></li>
+							<li><NuxtLink to="/1-corinthians">1 Corinthians</NuxtLink></li>
+							<li><NuxtLink to="/romans">Romans</NuxtLink></li>
+							<li><NuxtLink to="/acts">Acts</NuxtLink></li>
+							<li><NuxtLink to="/john">John</NuxtLink></li>
+							<li><NuxtLink to="/luke">Luke</NuxtLink></li>
+							<li><NuxtLink to="/mark">Mark</NuxtLink></li>
+							<li><NuxtLink to="/matthew">Matthew</NuxtLink></li>
 						</ol>
 					</section>
 				</amp-accordion>
