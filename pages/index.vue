5bbd148790ab9f9a33921609b46d604c6943863e
Tyler Trotter <tylertrotter@gmail.com>
2022-05-06 17:26:06 -0400

    Test

--- pages/index.vue
+++ pages/index.vue
@@ -22,6 +22,7 @@
           <li>Character profiles, chronology, geography, textual variants, cross-Scriptural citations, theological and ethical problems, applications</li>
           <li>The perfect resource for any Bible teacher</li>
           <li>Nothing on the internet is like this</li>
+          <li>Another point</li>
         </ul>
         <p-social-share />
         <h2 class="small-header">Comments? Corrections?</h2>
