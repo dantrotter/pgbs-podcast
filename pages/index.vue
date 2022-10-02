744601d8910261fef15ebe70ed44b518e7f88dca
Dan Trotter <dantrot@gmail.com>
2022-10-02 17:08:41 -0400

    Add podbean, YouTube to benefit bullets

--- pages/index.vue
+++ pages/index.vue
@@ -17,6 +17,8 @@
       </div>
       <div>
         <ul class="benefit-bullets">
+          <li><a href="https://dantrotter.podbean.com">Podbean podcasts</a> covering every verse of the NT (OT is under construction)</li>
+          <li><a href="https://www.youtube.com/channel/UCvdhN-S_y117F6p0V1nGR1Q/playlists">YouTube playlists</a> on five important theological topics</li>
           <li>Stream or download for free, ad-free</li>
           <li>Thousands of hours of research compiled into 40-minute audios</li>
           <li>High view of Scripture</li>
@@ -24,7 +26,6 @@
           <li>Character profiles, chronology, geography, textual variants, cross-Scriptural citations, theological and ethical problems, applications</li>
           <li>The perfect resource for any Bible teacher</li>
           <li>Nothing on the internet is like this</li>
-          <li> Old Testament is under construction</li>
         </ul>
         <p-social-share />
         <h2 class="small-header">Comments? Corrections?</h2>
